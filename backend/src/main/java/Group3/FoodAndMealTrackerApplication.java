802e08015b3b5b42b20a7b7db3d38776888c198c
Desiree Torres <desireetorres319@gmail.com>
2026-02-19 15:06:41 -0800

    Added file structure to backend files

---
+++ backend/src/main/java/Group3/FoodAndMealTrackerApplication.java
@@ -0,0 +1,13 @@
+package Group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAndMealTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAndMealTrackerApplication.class, args);
+	}
+
+}
