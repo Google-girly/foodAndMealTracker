8f114c73466178e0da0a0cc7c475722e331c3cd5
Omar-Martinez-F <omartinezfuentes@csumb.edu>
2026-02-27 15:35:51 -0800

    Fixed Timestamp in supa database

--- backend/src/main/java/Group3/model/Meal.java
+++ backend/src/main/java/Group3/model/Meal.java
@@ -2,11 +2,14 @@ package Group3.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 @Entity
@@ -20,7 +23,14 @@ public class Meal {
     private String mealType;
     private LocalDate mealDate;
     private String description;
+    
+
+    @CreationTimestamp
+    @Column(name = "created_at",updatable = false)
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
     public Meal(){}
